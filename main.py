5a0995dd0b56231436d0833911f60ce87d375477
Nid7239 <161845824+Nid7239@users.noreply.github.com>
2026-04-15 17:58:57 +0530

    updated

--- main.py
+++ main.py
@@ -2,31 +2,27 @@ import random
 from page_table import PageTable
 from constants import TOTAL_REQUESTS,PAGE_RANGE,OUTPUT_FILE
 
-
 def generate_sequence():
-    return [random.randint(1, PAGE_RANGE)for _ in range(TOTAL_REQUESTS)]
-
+    return [random.randint(1,PAGE_RANGE) for _ in range(TOTAL_REQUESTS)]
 
 def run():
     pt=PageTable()
     sequence=generate_sequence()
-
     hits=0
-    faults= 0
+    faults = 0
 
-    with open(OUTPUT_FILE,"w") as f:
-        f.write("lru page table Simulation\n")
-        f.write("=" *50+ "\n")
+    with open(OUTPUT_FILE, "w") as f:
+        f.write(" Page Table\n")
         f.write(f"Sequence:{sequence}\n\n")
-
         for page in sequence:
-            address, status = pt.access_page(page)
-            if status =="HIT":
-                hits += 1
+            file,status=pt.access_page(page)
+            if status == "HIT":
+                hits+= 1
             else:
-                faults += 1
-            frame_view =[entry[0] for entry in pt.frames]
-            f.write(f"Page {page} | {status} | Address {address} | Frames {frame_view}\n")
+                faults+= 1
+            # displat pages in memeory
+            frame_view=[entry[0] for entry in pt.frames]
+            f.write(f"Page {page} | {status} | File {file} | Frames {frame_view}\n")
         f.write("\n")
         f.write(f"Total Hits: {hits}\n")
         f.write(f"Total Faults: {faults}\n")
--- page_table.py
+++ page_table.py
@@ -1,37 +1,47 @@
 from constants import MAX_FRAMES
 
 class PageTable:
-    def __init__(self,max_frames=MAX_FRAMES):
-        self.max_frames=max_frames
+    def __init__(self, max_frames=MAX_FRAMES):
+        self.max_frames = max_frames #store no of frames
 
-        self.frames = [[None,0] for _ in range(max_frames)]
+
+        self.frames = [[None, 0] for _ in range(max_frames)] #memory sructure
+        #stores page,time 
         self.time = 0
-
-    # PUBLIC FUNCTION
-    def access_page(self,page):
-        self.time += 1
-
+        self.page_to_file = {}
+    # FUNCTION
+    def access_page(self, page):
+        self.time += 1 #for each page time increases
+        # HIT
         for i in range(self.max_frames):
             if self.frames[i][0] == page:
-                self.frames[i][1]=self.time  # update last access time
-                return i, "HIT"   # return address
+                self.frames[i][1] = self.time
+                file = self.page_to_file[page] #store the file address
+                return file, "HIT"
 
+        #if empty frame found then  insert page
         for i in range(self.max_frames):
             if self.frames[i][0] is None:
-                self.frames[i]=[page, self.time]
-                return i, "FAULT"
-
+                self.frames[i] = [page,self.time] #if empty insert 
+                self.page_to_file[page] = f"/var/data/page_{page}.data" #store file address
+                return self.page_to_file[page], "FAULT"
+        # LRU
         lru_index = self._find_lru()
-        self.frames[lru_index]=[page, self.time]
-        return lru_index, "FAULT"
 
+        old_page = self.frames[lru_index][0]
+        del self.page_to_file[old_page]
+
+        self.frames[lru_index] = [page, self.time]
+        self.page_to_file[page] = f"/var/data/page_{page}.data"
+        return self.page_to_file[page], "FAULT"
+
+    # INTERNAL FUNCTION
     def _find_lru(self):
-        min_time = self.frames[0][1]
-        lru_index = 0
-
+        min_time=self.frames[0][1]
+        lru_index=0
+#check which frame has smaleest access time
         for i in range(1,self.max_frames):
-            if self.frames[i][1] <min_time:
+            if self.frames[i][1]<min_time:
                 min_time=self.frames[i][1]
-                lru_index= i
-
+                lru_index=i
         return lru_index
